fbfc3406f8a8e104172da680ca5078a091a1157a
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-17 21:55:06 -0500

    feat(deps): Configure react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
+import { NotFoundPage } from "./pages";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
