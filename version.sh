7063ad3a61bcc8fda5cd5b7aa7c85ee0e9369124
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-19 08:51:32 -0500

    refactor: Update version script

--- version.sh
+++ version.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 VERSION=$(
-  cat package.json | 
-  grep version | 
-  head -1 |                 
-  awk -F: '{ print $2 }' |  # Split by ":" and get the second part
-  sed 's/[",]//g' |         # Remove quotes and commas
-  tr -d '[[:space:]]'       # Remove spaces
+  jq -r '.version' package.json
 )
 
 echo "version=$VERSION"
