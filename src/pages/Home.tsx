7a1ec4a94491a52fe14f5517644f4fa0b54d6206
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-23 15:41:36 -0500

    feat: Add infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { CharacterList } from "@/components/characters-list/CharacterList";
 import { getCharactersService } from "@/services/characters/get-characters.service";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useIntersectionObserver } from "@uidotdev/usehooks";
+import { Loader2 } from "lucide-react";
 
 export const Home = () => {
+  // Data fetching
   const { data, error, isLoading, isFetching, hasNextPage, fetchNextPage } =
     useInfiniteQuery({
       queryKey: ["characters"],
@@ -15,12 +18,27 @@ export const Home = () => {
 
   const characters = data?.pages.flatMap((page) => page.results) ?? [];
 
+  // Infinite scrolling functionality
+  const [intersectedElementRef, entry] = useIntersectionObserver({});
+  const isIntersecting = !!entry?.isIntersecting;
+  const canFetchMoreCharacters = !isLoading && !isFetching && hasNextPage;
+
+  if (isIntersecting && canFetchMoreCharacters) {
+    fetchNextPage();
+  }
+
   return (
     <main className="max-w-6xl mx-auto p-4">
       <CharacterList
         characters={characters}
         isLoading={isLoading || isFetching}
       />
+      {hasNextPage && (
+        <Loader2
+          ref={intersectedElementRef}
+          className="text-primary animate-spin mx-auto mt-4"
+        />
+      )}
     </main>
   );
 };
